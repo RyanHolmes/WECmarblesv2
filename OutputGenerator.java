9d017a5536d3ce1f81d715461d6a581591e2b11c
Hannes Filler <hannes.filler@live.com>
2015-01-17 00:57:42 -0500

    Done

--- OutputGenerator.java
+++ OutputGenerator.java
@@ -10,7 +10,7 @@ import java.util.*;
 public class OutputGenerator {
 	public static ArrayList<ArrayList<item>> results;
 	public static void generateOutput(int numColours, StringBuilder outputStream) {
-		//outputStream.append("<html>");
+		outputStream.append("&");
 		//outputStream.append(numColours + " was inputted.");
 		//outputStream.append("</html>");
 		results = new ArrayList<ArrayList<item>>();
@@ -19,11 +19,31 @@ public class OutputGenerator {
 			//if we're doing the first thing.
 			mySet.add(new item(i));
 			//if we're doing the second thing.
+			for(int j=1; j<=i; j++){
+				mySet.add(new item(i,j-1));
+			}
 		}
 		allSets(new ArrayList<item>(),mySet);
 		System.out.println(results);
 		//string things.
-		outputStream.append(results);
+		Collections.sort(results, new Comparator<ArrayList<item>> () {
+
+			@Override
+			public int compare(ArrayList<item> a, ArrayList<item> b) {
+				if(a.size() > b.size()){
+					return 1;
+				} else 
+					return 0;
+				}
+			});
+		for(int i=0; i<results.size(); i++){
+			for(int j=0; j<results.get(i).size();j++){
+				outputStream.append("<td class='Circle"+results.get(i).get(j)+"'/>"+results.get(i).get(j)+"<td>");
+			}
+			outputStream.append("<br/>");
+		}
+		outputStream.append("&");
+		//outputStream.append(results);
 	}
 	public static class item{
 		public int color;
